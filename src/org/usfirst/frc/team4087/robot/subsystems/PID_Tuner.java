fa1be32c34876ad159f82acd48272c7c86856f4c
Kraig Oramous <kraigamous@gmail.com>
2018-10-19 11:11:24 -0500

    experimental uppage of P

--- src/org/usfirst/frc/team4087/robot/subsystems/PID_Tuner.java
+++ src/org/usfirst/frc/team4087/robot/subsystems/PID_Tuner.java
@@ -18,16 +18,28 @@ public class PID_Tuner extends Subsystem {
 	double PrevPositionCounter = 0;
 
 	double OscillationPeriodTimer = 0;
-	
+
 	double Period = 0;
+
+	double P = 0.0000001;
 
 	long startTime = 0;
 	long endTime = 0;
 
+	// added all this crap with P
+
 	public double P_Tuner() {
 
-		return .002;
-		// .0001
+		if (ifOscillating(Robot.winch.getWinchPosition(), 8000) == false && OscillationCounter <= 0) {
+
+			return P *= 10;
+
+		} else {
+
+			return P;
+
+		}
+
 	}
 
 	public double I_Tuner() {
@@ -40,11 +52,10 @@ public class PID_Tuner extends Subsystem {
 
 	@Override
 	protected void initDefaultCommand() {
-		// TODO Auto-generated method stub
 
 	}
 
-	public double ifOscillating(double CurrentPosition, double setpoint) {
+	public boolean ifOscillating(double CurrentPosition, double setpoint) {
 
 		SmartDashboard.putNumber("Period", Period);
 		SmartDashboard.putNumber("Oscillation Counter", OscillationCounter);
@@ -58,16 +69,16 @@ public class PID_Tuner extends Subsystem {
 
 			PrevPositionCounter = PositionCounter;
 
-			if (PrevOscillationCounter == OscillationCounter - 1 ) {
+			if (PrevOscillationCounter == OscillationCounter - 1) {
 
 				PrevOscillationCounter++;
 			}
 		}
-		
+
 		if (PositionCounter > PrevPositionCounter && OscillationCounter == PrevOscillationCounter) {
 			OscillationCounter++;
 		}
-		
+
 		if (OscillationCounter == 3) {
 
 			startTime = System.currentTimeMillis();
@@ -77,27 +88,28 @@ public class PID_Tuner extends Subsystem {
 
 			endTime = System.currentTimeMillis();
 		}
-		
-		Period = (endTime-startTime)/.02;
+		// added thing here to reset the thing
+		if (ifOscillating(CurrentPosition, setpoint) == false) {
+			OscillationCounter = 0;
+		}
+
+		if (OscillationCounter > 2) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	public double returnPeriod(boolean ifOscillating) {
+
+		Period = (endTime - startTime) / .02;
 		return Period;
+
 	}
 }
 
 /*
  * public int randomSetpoint() { Random rand = new Random(); int rand1 =
  * rand.nextInt(-35000); return rand1; }
- */
-
-/*
- * See NI Software link -- Maybe a better way to measure oscillation would be to
- * transfer the range of values into a sigmoid or some other function that makes
- * it binary or close to binary, to trigger within a certain range, then measure
- * that triggering? Kinda similar to how we do it now but different at the same
- * time I'd suggest look at the NI link I sent to you, there's some good stuff
- * there about measuring the oscillation, although they use a frequency rather
- * than a physical oscillation which is slightly different in principle We also
- * should probably think about graphing the motion of the elevator over time,
- * which might help us figure out exactly what the sensor is measuring and
- * classifying as "oscillation" That's a big one, graphing it -- fairly easy if
- * we find the right libraries and run the program through the correct processor
  */
